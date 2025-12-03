27fd06a5067fe247126f56167a0584f7922d0cc3
Angelica <lisoveckaya1997@gmail.com>
2025-12-03 10:45:09 +0200

    ю

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,9 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  const links = document.querySelectorAll('.js-site-nav_link');
+  
+
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -15,10 +18,10 @@
     closeMenuBtn.getAttribute("aria-expanded") === "true" || false;
     closeMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
-
-    const scrollLockMethod = !isMenuOpen ? disableBodyScroll : enableBodyScroll;
-
-    scrollLockMethod(document.body);
+   const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
@@ -31,4 +34,30 @@
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    // Закриваємо меню
+    mobileMenu.classList.remove("is-open");
+
+    // Увімкнути скрол
+    bodyScrollLock.enableBodyScroll(document.body);
+
+    // Дати трішки часу, щоб анімація меню закрилась
+    setTimeout(() => {
+      const href = link.getAttribute('href');
+      if (href.startsWith('#')) {
+        document.querySelector(href).scrollIntoView({
+          behavior: "smooth"
+        });
+      } else {
+        window.location.href = href;
+      }
+    }, 100); 
+  });
+});
+
+
 })();
+
+
