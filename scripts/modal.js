cd667b7435d08ea6b2da17edf2adc3631d549bfa
Angelica <lisoveckaya1997@gmail.com>
2025-12-03 14:31:52 +0200

    .

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,6 +3,9 @@
       openModalBtn: document.querySelectorAll("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      mobileMenu: document.querySelector(".js-menu-container"),
+    openMenuBtn: document.querySelector(".js-open-menu"),
+   closeMenuBtn: document.querySelector(".js-close-menu"),
     };
   
    refs.openModalBtn.forEach(btn => {
@@ -13,10 +16,21 @@
     function toggleModal() {
       document.body.classList.toggle('modal-open');
       refs.modal.classList.toggle("is-hidden");
+      refs.mobileMenu.classList.remove("is-open");
+      refs.closeMenuBtn.setAttribute("aria-expanded", false);
+        refs.openMenuBtn.setAttribute("aria-expanded", false);
+
+         const isMenuOpen =
+      refs.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+      console.log(isMenuOpen)
+        const scrollLockMethod = isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
     }
 
-    modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
+    refs.modal.addEventListener('click', (e) => {
+  if (e.target === refs.modal) {
     modal.style.display = 'none';
     document.body.classList.remove('modal-open');
   }
